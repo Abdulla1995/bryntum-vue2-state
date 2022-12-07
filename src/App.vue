83438450b6e633990bf9f2c1c3c7866ecf203668
abdulla1995 <emciyevabdulla@gmail.com>
2022-12-07 12:53:41 +0400

    Bryntum test created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <bryntum-grid
+        ref="grid"
+        v-bind="gridConfig"
+        :data="filteredItems"
+        class="flex-1"
+    />
+    <button @click="saveState" style="margin-top: 20px; padding: 5px 8px;">Save state</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { BryntumGrid } from '@bryntum/grid-vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    BryntumGrid
+  },
+  data () {
+    return {
+      gridConfig: {
+        rowHeight: 60,
+        minHeight: '20em',
+        selectionMode: {
+          checkbox: true,
+          showCheckAll: true,
+          row: false
+        },
+        columnLines: false,
+        columns: [
+          {
+            flex: 1,
+            htmlEncode: false,
+            editor: false,
+            filterable: false,
+            text: 'Full name',
+            field: 'fullName',
+          },
+          {
+            text: 'Email',
+            htmlEncode: false,
+            renderer ({ record }) {
+              return `<div class="text-blue-500">${record.email}</div>`
+            },
+            flex: 1
+          },
+          {
+            text: 'Phone Number',
+            htmlEncode: false,
+            renderer ({ record }) {
+              return `<div class="text-blue-500">${record.phone}</div>`
+            },
+            flex: 1
+          }
+        ]
+      },
+      gridInstance: null,
+      gridStateId: 'grid.state-id',
+      filteredItems: []
+    }
+  },
+  created() {
+    for(let i = 1; i < 10; i ++ ) {
+      this.filteredItems.push({
+        fullName: 'Tony Gonzales 1' + i ,
+        email: 'ton@gmail.com',
+        phone: '12321' + i
+      })
+    }
+  },
+  mounted() {
+    this.gridInstance = this.$refs.grid.instance
+    // console.log(this.gridInstance.isStateful)
+    if (this.gridInstance) {
+      const savedState = localStorage.getItem(this.gridStateId)
+      if (savedState && this.gridInstance) {
+        const parsedState = JSON.parse(savedState)
+        this.gridInstance.applyState(parsedState)
+      }
+    }
+  },
+  methods: {
+    saveState() {
+      // gridInstance.saveState()
+      // actually this state should give me 3 columns when I remove Phone number from the list but it provides 4 of them
+      // that's why I just splice and copied to get only 2 columns and then apply them on mounted hook.
+      const stateColumnsCopy = this.gridInstance.state.columns.splice(1, 2)
+      this.gridInstance.state.columns = stateColumnsCopy
+      const copiedInstance = { ...this.gridInstance.state }
+      copiedInstance.columns = stateColumnsCopy
+      localStorage.setItem(this.gridStateId, JSON.stringify(copiedInstance))
+    },
+    resetState() {
+      // gridInstance.resetDefaultState()
+      localStorage.removeItem(this.gridStateId)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import '@bryntum/grid/grid.stockholm.css'
 
 new Vue({
   render: h => h(App),
